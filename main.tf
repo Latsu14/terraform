7fb017bb679620df9d33cefdbc1ff5819e71b55d
Latsu14 <80422798+Latsu14@users.noreply.github.com>
2026-07-16 13:48:56 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,16 @@ module "vpc" {
   }
 }
 
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-12334999"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
