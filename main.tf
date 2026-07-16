fc7b92efc4e85df4b5cc3159a09fa942cd966a65
Latsu14 <80422798+Latsu14@users.noreply.github.com>
2026-07-16 13:18:05 +0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-central-1a", "eu-central-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name      = "single-instance"
+  ami       = "ami-028acd58719b05107"
+  subnet_id = "subnet-0663095636a751618"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "s3_bucket" {
+  source      = "./modules/s3"
+  bucket_name = "ragaca12345"
+}
